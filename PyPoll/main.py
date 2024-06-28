c787186b86d1101d5be1faf57048db9177459c79
foshaystephanie <foshaystephanie@gmail.com>
2024-06-27 21:05:43 -0500

    Finished logic for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,4 +8,55 @@ with open(election_data, 'r') as csvfile:
     next(csv_reader)
 
     total_votes = 0
-    votes = {}
+    candidates = {}
+
+    for row in csv_reader:
+        total_votes += 1
+
+        candidate = row[2]
+
+        if candidate in candidates:
+            candidates[candidate] += 1
+        else:
+            candidates[candidate] = 1
+
+election_results = 'analysis/election_results.txt'
+
+with open (election_results, 'w') as output:
+    print("Election Results")
+    print("")
+    print("-------------------------")
+    print("")
+    print(f"Total Votes: {total_votes}")
+    print("")
+    print("-------------------------")
+    print("")
+
+    output.write("Election Results\n")
+    output.write("\n")
+    output.write("-------------------------\n")
+    output.write("\n")
+    output.write(f"Total Votes: {total_votes}\n")
+    output.write("\n")
+    output.write("-------------------------\n")
+    output.write("\n")
+
+    for candidate, votes in candidates.items():
+        percentage = (votes / total_votes) * 100
+        print(f"{candidate}: {percentage:.3f}% ({votes})")
+        output.write(f"{candidate}: {percentage:.3f}% ({votes})\n")
+
+    winner = max(candidates, key = candidates.get)
+
+    print("")
+    print("-------------------------")
+    print("")
+    print(f"Winner: {winner}")
+    print("")
+    print("-------------------------")
+    print("")
+    output.write("-------------------------\n")
+    print("")
+    output.write(f"Winner: {winner}\n")
+    print("")
+    output.write("-------------------------\n")
